8f6cddf7ea9a6538683045cf4871437103e20a94
Per Eriksson <per_hegg@hotmail.com>
2020-01-30 14:38:15 +0100

    add new font and responsivness

--- src/About.jsx
+++ src/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
                     <p className="about__content-text">
                         My name is Per Eriksson and I'm a Junior Full Stack Developer with a passion for computers, music, health and so much more. 
                         I am opera singer with a master degree and have been working since 2015 but I've decided to switch career and become a developer.
-
-                        In 2019 I took a 12 week Full stack Developer boot camp at Craft Academy. The course was very intense and I learnt more than I could wish for. The focus was on 
+                        In 2019 I took a 12 week Full stack Developer boot camp at Craft Academy. 
+                        The course was very intense and I learnt more than I could wish for. The focus was on 
                         agile development with morning scrum and also test driven development where you write the tests before doing any coding.  
-                        We worked mainly with Ruby on Rails as back end and React as front end. After the course I continued to take courses in javascript, css, python, sql etc.
+                        We worked mainly with Ruby on Rails as back end and React as front end. <br/> <br/>
+                        After the course I continued to take courses in javascript, css, python, sql etc.
                         I really feel that I'm ready to start working as a developer and to dive deeper into the world of code.   
-
-                        As a developer I'm a quick learner and eager to learn new things every day. I have very good patience and don't frustrated in the first couple or errors...
-
+                        As a developer I'm a quick learner and eager to learn new things every day. 
+                        I have very good patience and don't frustrated in the first couple or errors...
                         From my work as a singer I've met many different kind of people and learnt how to deal with differnt personalities to make the situation as good as possible.
 
                     </p>
@@ -48,10 +48,10 @@ const About = () => {
 
                 <div className="about__item--5">
                     <h4 className="about__item--5--title">EDUCATION</h4>
-                    <p className="about__item--5--text">
+                    <div className="about__item--5--text">
                        <p>&diams; 12 week Full Stack Developer Boot Camp</p>
                        <p>&diams; 2 years of self studies</p>
-                    </p>
+                    </div>
                 </div>
 
 
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,10 +4,10 @@ const Contact = () => {
     return (
         <div className="contact__container">
             <div className="contact">
-                <div className="contact__bild"></div>
-                <div className="contact__name">Name: Per Eriksson</div>
-                <div className="contact__telefon">Telephone: +46707289554</div>
-                <div className="contact__mail">Email: per_hegg@hotmail.com</div>
+                <div className="contact__text contact__name">Name: Per Eriksson</div>
+                <div className="contact__text contact__bild"></div>
+                <div className="contact__text contact__telefon">Telephone: +46707289554</div>
+                <div className="contact__text contact__mail">Email: per_hegg@hotmail.com</div>
                 <a href="https://www.linkedin.com/in/per-eriksson-537761a8/" target="_blank" rel="noopener noreferrer" className="contact__linkedin">
                     <div className="icons__linkedin"></div>
                 </a>
